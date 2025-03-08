0fb5bed72c9b93c52ad334412de891d227568d35
Moleboheng Mahlatji <molebohengmahlatji@gmail.com>
2025-03-08 06:01:21 +0200

    fixed isues with db models

--- models/user.py
+++ models/user.py
@@ -1,1 +1,8 @@
 ### this is where we record data about the user and their unique identifier
+from models import db
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    loginKey = db.Column(db.String(4), nullable=False, unique=True)
+    
+    def __repr__(self):
+        return "<loginKey %s>" % self.loginKey
