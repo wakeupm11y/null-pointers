ae2c4429e0847a4d3ab7f61730dd96356dfe725c
Moleboheng Mahlatji <molebohengmahlatji@gmail.com>
2025-03-08 03:24:02 +0200

    upload page looks good

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for
+from routes.auth import auth
+from routes.upload import upload
+
+app = Flask(__name__)
+
+app.register_blueprint(auth)
+app.register_blueprint(upload)
+
+
+@app.route("/")
+def home():
+    return render_template("upload.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
